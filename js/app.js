7b573d1d0764d6135cecf3e1059168e25b46c9ad
Tyler Dillon <harrell_t@icloud.com>
2019-06-14 09:42:50 -0400

    Update app.js
    Added keyboard functionality

--- js/app.js
+++ js/app.js
@@ -1,15 +1,6 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
- * app.js */
-
- //Creates new instance of game, adds event listeners for start button and onscreen keyboard buttons
-
-
-//  My getRandomPhase method returns an object literal with a phase property, so this code worked on step 6
-//  const game = new Game(); 
-//  const randomPhrase = game.getRandomPhrase(); 
-//  const phrase = new Phrase(randomPhrase.phrase); 
-//  phrase.addPhraseToDisplay(); 
+ * app.js */ 
 
 let game;
 document.getElementById('btn__reset').addEventListener('click', () => {
@@ -19,6 +10,20 @@ document.getElementById('btn__reset').addEventListener('click', () => {
 
 document.getElementById('qwerty').addEventListener('click', (e) => {
     if (e.target.nodeName == "BUTTON"){
+        console.log(e.target)
        game.handleInteraction(e.target);
     }
 })
+
+document.addEventListener("keypress", (e) => {
+    let qwerty = document.getElementsByClassName('key');
+    if (e.keyCode === 13 && document.getElementById('overlay').style.display !== "none") {
+        game = new Game();
+        game.startGame();
+    }
+    for (let i = 0; i < qwerty.length; i++) {
+        if (e.key === qwerty[i].textContent) {
+            game.handleInteraction(qwerty[i]);
+        } 
+    }
+});
