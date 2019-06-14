ad9d61faaeb365521ae3f633879bfbbee59ced6b
Tyler Dillon <harrell_t@icloud.com>
2019-06-14 09:49:30 -0400

    added comments and refactored

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * app.js */ 
-
+ // Creates new instance of game
 let game;
+
+//Gives functionality to reset button
 document.getElementById('btn__reset').addEventListener('click', () => {
     game = new Game();
     game.startGame();
 })
 
+//Gives functionality to keyboard buttons on web app
 document.getElementById('qwerty').addEventListener('click', (e) => {
     if (e.target.nodeName == "BUTTON"){
         console.log(e.target)
@@ -15,15 +15,16 @@ document.getElementById('qwerty').addEventListener('click', (e) => {
     }
 })
 
+//Give functionality to user's keyboard
 document.addEventListener("keypress", (e) => {
     let qwerty = document.getElementsByClassName('key');
+    //event listener for enter key. Only works if the 'start game' button and screen is displayed
     if (e.keyCode === 13 && document.getElementById('overlay').style.display !== "none") {
         game = new Game();
         game.startGame();
     }
+    //event listener for the letters on the keys. Only works for actual letters represented on the screen
     for (let i = 0; i < qwerty.length; i++) {
-        if (e.key === qwerty[i].textContent) {
-            game.handleInteraction(qwerty[i]);
-        } 
+        e.key === qwerty[i].textContent ? game.handleInteraction(qwerty[i]) : false;
     }
 });
